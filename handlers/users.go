2b2599d6f746fb9b98ff0e313a13baf9700e78fc
hccccrr <vrajiv830@gmail.com>
2026-02-17 22:31:07 +0530

    Update users.go

--- handlers/users.go
+++ handlers/users.go
@@ -3,24 +3,19 @@ package handlers
 import (
 	tg "github.com/amarnathcjd/gogram/telegram"
 	"shizumusic/core"
-	"shizumusic/utils"
-	"shizumusic/helpers"
 )
-
-// TEXTS templates from utils
-var TEXTS = helpers.TextTemplates
 
 func RegisterUserHandlers(client *core.Client, db *core.Database) {
 	// Profile/Me
 	client.BotClient.AddMessageHandler("/me", handleProfile(db))
 	client.BotClient.AddMessageHandler("/profile", handleProfile(db))
-	
+
 	// Stats
 	client.BotClient.AddMessageHandler("/stats", handleStats(db))
-	
-	// Leaderboard
-	client.BotClient.AddMessageHandler("/leaderboard", handleLeaderboard(db))
-	client.BotClient.AddMessageHandler("/topusers", handleLeaderboard(db))
+
+	// Leaderboard - handled by leaderboard_handler.go
+	client.BotClient.AddMessageHandler("/leaderboard", handleLeaderboard(client, db))
+	client.BotClient.AddMessageHandler("/topusers", handleLeaderboard(client, db))
 }
 
 func handleProfile(db *core.Database) core.HandlerFunc {
@@ -33,13 +28,6 @@ func handleProfile(db *core.Database) core.HandlerFunc {
 func handleStats(db *core.Database) core.HandlerFunc {
 	return func(m *tg.NewMessage) error {
 		m.Reply("**📊 Bot Statistics**\n\nFeature coming soon!")
-		return nil
-	}
-}
-
-func handleLeaderboard(db *core.Database) core.HandlerFunc {
-	return func(m *tg.NewMessage) error {
-		m.Reply("**🏆 Leaderboard**\n\nFeature coming soon!")
 		return nil
 	}
 }
