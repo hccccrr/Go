511faf7d94e1379d8bf8583b26e77df88106078f
hccccrr <vrajiv830@gmail.com>
2026-02-17 22:32:31 +0530

    Create init.go

---
+++ handlers/init.go
@@ -0,0 +1,7 @@
+package handlers
+
+import "shizumusic/helpers"
+
+// TEXTS - single declaration for entire handlers package
+// Sab handler files isko use kar sakde han bina redeclare kite
+var TEXTS = helpers.TextTemplates
