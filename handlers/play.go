f6713f15bc03bebb55479475dc5034cdcc2cbcfa
hccccrr <vrajiv830@gmail.com>
2026-02-18 11:58:46 +0530

    Update play.go

--- handlers/play.go
+++ handlers/play.go
@@ -1,52 +1,103 @@
 package handlers
 
 import (
+	"fmt"
+
 	tg "github.com/amarnathcjd/gogram/telegram"
+	"shizumusic/config"
 	"shizumusic/core"
+	"shizumusic/helpers"
 )
 
-func RegisterPlayHandlers(client *core.Client, db *core.Database, calls *core.Calls) {
-	// Play commands
-	client.BotClient.AddMessageHandler("/play", func(m *tg.NewMessage) error {
-		return handlePlay(client, db, calls)(m)
-	})
-	client.BotClient.AddMessageHandler("/vplay", func(m *tg.NewMessage) error {
-		return handleVPlay(client, db, calls)(m)
-	})
+func init() {
+	RegisterPlugin("play_commands", func(client *core.Client, db *core.Database) {
+		calls := core.NewCalls(client.UserClient)
 
-	// Queue
-	client.BotClient.AddMessageHandler("/queue", func(m *tg.NewMessage) error {
-		return handleQueue(db)(m)
-	})
-	client.BotClient.AddMessageHandler("/current", func(m *tg.NewMessage) error {
-		return handleCurrent(db)(m)
+		client.BotClient.AddMessageHandler("/play", func(m *tg.NewMessage) error {
+			return handlePlay(m, client, db, calls)
+		})
+		client.BotClient.AddMessageHandler("/vplay", func(m *tg.NewMessage) error {
+			return handleVPlay(m, client, db, calls)
+		})
+		client.BotClient.AddMessageHandler("/queue", func(m *tg.NewMessage) error {
+			return handleQueue(m, db)
+		})
+		client.BotClient.AddMessageHandler("/current", func(m *tg.NewMessage) error {
+			return handleCurrent(m, db)
+		})
 	})
 }
 
-func handlePlay(client *core.Client, db *core.Database, calls *core.Calls) tg.MessageHandler {
-	return func(m *tg.NewMessage) error {
-		m.Reply("**🎵 Play Feature**\n\nComing soon!")
+func handlePlay(m *tg.NewMessage, client *core.Client, db *core.Database, calls *core.Calls) error {
+	sender, err := m.GetSender()
+	if err != nil || sender == nil {
 		return nil
 	}
+	if config.Cfg.IsBanned(sender.ID) {
+		return nil
+	}
+	if !m.IsGroup() {
+		return nil
+	}
+
+	me, _ := client.BotClient.GetMe()
+	mention := fmt.Sprintf("[%s](tg://user?id=%d)", sender.FirstName, sender.ID)
+	text := fmt.Sprintf(helpers.TextTemplates.PlayReply(), mention, fmt.Sprintf("@%s", me.Username))
+	btns := helpers.Buttons.PlayerMarkup(m.ChatID(), "coming_soon", me.Username)
+	m.Reply(text, tg.SendOptions{ReplyMarkup: btns})
+	return nil
 }
 
-func handleVPlay(client *core.Client, db *core.Database, calls *core.Calls) tg.MessageHandler {
-	return func(m *tg.NewMessage) error {
-		m.Reply("**📹 Video Play Feature**\n\nComing soon!")
+func handleVPlay(m *tg.NewMessage, client *core.Client, db *core.Database, calls *core.Calls) error {
+	sender, err := m.GetSender()
+	if err != nil || sender == nil {
 		return nil
 	}
+	if config.Cfg.IsBanned(sender.ID) {
+		return nil
+	}
+	if !m.IsGroup() {
+		return nil
+	}
+
+	me, _ := client.BotClient.GetMe()
+	mention := fmt.Sprintf("[%s](tg://user?id=%d)", sender.FirstName, sender.ID)
+	text := fmt.Sprintf(helpers.TextTemplates.VPlayReply(), mention, fmt.Sprintf("@%s", me.Username))
+	btns := helpers.Buttons.PlayerMarkup(m.ChatID(), "coming_soon", me.Username)
+	m.Reply(text, tg.SendOptions{ReplyMarkup: btns})
+	return nil
 }
 
-func handleQueue(db *core.Database) tg.MessageHandler {
-	return func(m *tg.NewMessage) error {
-		m.Reply("**📋 Queue**\n\nNo songs in queue!")
+func handleQueue(m *tg.NewMessage, db *core.Database) error {
+	sender, err := m.GetSender()
+	if err != nil || sender == nil {
 		return nil
 	}
+	if config.Cfg.IsBanned(sender.ID) {
+		return nil
+	}
+	if !m.IsGroup() {
+		return nil
+	}
+
+	btns := helpers.Buttons.QueueMarkup(1, 0)
+	m.Reply(helpers.TextTemplates.QueueEmpty(), tg.SendOptions{ReplyMarkup: btns})
+	return nil
 }
 
-func handleCurrent(db *core.Database) tg.MessageHandler {
-	return func(m *tg.NewMessage) error {
-		m.Reply("**🎧 Current Song**\n\nNothing is playing!")
+func handleCurrent(m *tg.NewMessage, db *core.Database) error {
+	sender, err := m.GetSender()
+	if err != nil || sender == nil {
 		return nil
 	}
+	if config.Cfg.IsBanned(sender.ID) {
+		return nil
+	}
+	if !m.IsGroup() {
+		return nil
+	}
+
+	btns := helpers.Buttons.CloseMarkup()
+	m.Reply(helpers.TextTemplates.NothingPlaying(), tg.SendOptions{ReplyMarkup: btns})
+	return nil
 }
